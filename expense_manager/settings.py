53403f2d42854a8fb915a5f1264b0d9083602b53
AnishSangrulkar <anish.sangrulkar@gmail.com>
2021-05-08 20:25:55 +0530

    5 - removed decouple module

--- expense_manager/settings.py
+++ expense_manager/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY") 
+SECRET_KEY = 'y2(s2gjr*#g27d7a!v+_&(32ah8^=xo(o4l&=f9qbgqpnfx%jp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
